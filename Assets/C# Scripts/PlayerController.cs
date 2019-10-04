8a3892a7f41e9336bad9afb4aeaec3c5592ad74c
CJMathaniel <32065287+CjayGamerX@users.noreply.github.com>
2019-10-04 09:17:04 -0400

    Initial commit

---
+++ Assets/C# Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Private Variable
+    private float speed = 5.0f;
+    private float turnSpeed;
+    private float horizontalInput;
+    private float verticalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Player Input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //Make a vehicle move different directions every single frame
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
